0a1f92f1852ff341b4c7520c93402c7a69333695
Joshua-Rose <joshua.rose1@outlook.com>
2020-03-21 20:17:44 -0500

    Putting Styled SVGs in the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
 import * as React from 'react'
-import Crane from '../assets/Crane.svg'
+import StyledSVG, {
+  Bash,
+  Bootstrap,
+  CSS3,
+  Git,
+  Github,
+  HTML5,
+  JoshuaAtDesk,
+  JrGiantLogo,
+  LESS,
+  Node,
+  Npm,
+  ReactSVG,
+  Sass,
+  verdaccio as Verdaccio
+} from '../components/svgs'
 
 import Page from '../components/Page'
 import Container from '../components/Container'
@@ -12,7 +27,12 @@ const IndexPage = () => (
         <h1>Greetings programs</h1>
         <p>Welcome to our new site.</p>
         <p>We are still in development.</p>
-        <img src={Crane} alt="Crane" />
+        {[Bash, Bootstrap, CSS3, Git, Github, HTML5, JrGiantLogo, LESS, Node, Npm, ReactSVG, Sass, Verdaccio].map(S => (
+          <StyledSVG className={S.name}>
+            <S />
+          </StyledSVG>
+        ))}
+        <JoshuaAtDesk />
       </Container>
     </Page>
   </IndexLayout>
