386bbd44748bb767d028a8e93a736b07ace778d0
Joshua-Rose <joshua.rose1@outlook.com>
2020-03-21 20:17:23 -0500

    💄Adding style

---
+++ src/components/svgs/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import styled from '@emotion/styled'
+import { getEmSize } from '../../styles/mixins'
+import Bash from './Bash'
+import Bootstrap from './Bootstrap'
+import CSS3 from './CSS3'
+import Git from './Git'
+import Github from './Github'
+import HTML5 from './HTML5'
+// import JavaScript from './JavaScript'
+import JoshuaAtDesk from './JoshuaAtDesk'
+import JrGiantLogo from './JrGiantLogo'
+import LESS from './LESS'
+import Node from './Node'
+import Npm from './Npm'
+import ReactSVG from './React'
+import Sass from './Sass'
+import verdaccio from './Verdaccio'
+
+export { Bash, Bootstrap, CSS3, Git, Github, HTML5, JoshuaAtDesk, JrGiantLogo, LESS, Node, Npm, ReactSVG, Sass, verdaccio }
+const StyledSVG = styled.div`
+  max-width: ${getEmSize(200)}em;
+  svg {
+    width: 100%;
+  }
+`
+interface SVGProps {
+  className?: string
+}
+
+const SVG: React.FC<SVGProps> = ({ children, className }) => <StyledSVG className={className}>{children}</StyledSVG>
+
+export default SVG
