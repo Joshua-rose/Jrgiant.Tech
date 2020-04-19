5ab61e453bfc502eb60d6636754e86b441e90ca4
Joshua-Rose <joshua.rose1@outlook.com>
2020-04-18 23:18:13 -0500

    fix ts error 2307

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -13,3 +13,12 @@ declare module '*.module.css' {
   const cssModule: CSSModule
   export = cssModule
 }
+
+declare module '*.svg' {
+  const content: any
+  export const ReactComponent: any
+  export default content
+}
+
+declare module '*.png'
+declare module '*.jpg'
