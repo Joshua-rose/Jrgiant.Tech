78b1c2761f7aa70d679dea881850183c99e2dd44
Joshua-Rose <joshua.rose1@outlook.com>
2020-04-29 15:42:26 -0500

    Adding Styles

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,24 +1,58 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from 'react'
 import Recaptcha from 'react-recaptcha'
 import styled from '@emotion/styled'
-import { eventNames } from 'cluster'
 import AnimatedEnvelop from '../assets/Animated Envelop With Text.svg'
 
 import Page from '../components/Page'
 import Container from '../components/Container'
 import IndexLayout from '../layouts'
+import { colors } from '../styles/variables'
 
 const FormContainer = styled.div`
-  display: grid;
-  grid-template-columns: 3fr 2fr;
+  display: flex;
+  min-height: 80vh;
+  flex-wrap: wrap;
+  width: 100%;
   align-items: center;
+  *.hidden {
+    display: none;
+  }
+  .hidden-label-text {
+    position: absolute;
+    left: -10000px;
+  }
+  input,
+  textarea,
+  button {
+    width: 90%;
+    border-radius: 6px;
+    padding: 0 1rem;
+  }
+  input,
+  button {
+    min-height: 3rem;
+  }
+  textarea {
+    min-height: 10rem;
+    padding-top: 1rem;
+  }
+  button {
+    background-color: ${colors.brand};
+    color: ${colors.white};
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.4rem;
+  }
 `
 const FormSection = styled.div`
-  grid-column: 1;
+  flex: 3;
+  min-width: 350px;
 `
 
 const SvgSection = styled.div`
-  grid-column: 2;
+  flex: 2;
+  min-width: 200px;
 `
 
 export default function Contact() {
@@ -28,32 +62,31 @@ export default function Contact() {
     message: ''
   })
 
-  const verifyCallback = (data: any) => {
-    console.log('verify Callback', data)
-  }
-
+  const [showReCaptcha, setShowReCaptcha] = useState(false)
   const [isRecaptchaLoaded, setIsRecaptchaLoaded] = useState(false)
   const recaptchaLoaded = () => {
     setIsRecaptchaLoaded(true)
   }
-  const [isValidForm, setIsValidForm] = useState(false)
-
-  const encode = (data: any) => {
-    return Object.keys(data)
-      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-      .join('&')
-  }
+  // const [isValidForm, setIsValidForm] = useState(false)
 
   const handleSubmit = (e: any) => {
     e.preventDefault()
-    if (isValidForm)
-      fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: encode({ 'form-name': 'contact', ...form })
-      })
-        .then(() => alert('Success!'))
-        .catch(error => alert(error))
+    setShowReCaptcha(true)
+  }
+  const encode = (data: { 'form-name': string; name: string; email: string; message: string; 'g-recaptcha-response': string }) => {
+    return Object.keys(data)
+      .map((key: any) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      .join('&')
+  }
+  const verifyCallback = (data: string) => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...form, 'g-recaptcha-response': data })
+    })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error))
+    // console.log('verify Callback', data)
   }
 
   const handleChange = (e: any) => {
@@ -70,40 +103,54 @@ export default function Contact() {
         <Container>
           <FormContainer>
             <FormSection>
-              <form onSubmit={handleSubmit}>
-                {isRecaptchaLoaded ? (
+              <h1>Contact Us</h1>
+              {!isRecaptchaLoaded && (
+                <form
+                  onSubmit={handleSubmit}
+                  name="contact"
+                  method="POST"
+                  data-netlify-recaptcha="true"
+                  netlify-honeypot="bot-field"
+                  data-netlify="true"
+                >
+                  <p className="hidden">
+                    <label htmlFor="bot-field">
+                      Don’t fill this out if you're human: <input name="bot-field" id="bot-field" />
+                    </label>
+                  </p>
                   <div>
                     <p>
                       <label htmlFor="name">
-                        Your Name: <input type="text" name="name" id="name" value={form.name} onChange={handleChange} />
+                        <span className="hidden-label-text">Your Name: </span>
+                        <input type="text" name="name" id="name" value={form.name} onChange={handleChange} placeholder="Name" />
                       </label>
                     </p>
                     <p>
                       <label htmlFor="email">
-                        Your Email: <input type="email" name="email" id="email" value={form.email} onChange={handleChange} />
+                        <span className="hidden-label-text">Your Email: </span>
+                        <input type="email" name="email" id="email" value={form.email} onChange={handleChange} placeholder="Email" />
                       </label>
                     </p>
                     <p>
                       <label htmlFor="message">
-                        Message: <textarea name="message" id="message" value={form.message} onChange={handleChange} />
+                        <span className="hidden-label-text">Message: </span>
+                        <textarea name="message" id="message" value={form.message} onChange={handleChange} placeholder="Message" />
                       </label>
                     </p>
                   </div>
-                ) : (
-                    <div />
-                  )}
+                  <p>
+                    <button type="submit">Send</button>
+                  </p>
+                </form>
+              )}
+              {showReCaptcha && (
                 <Recaptcha
-                  sitekey="6Ldnxe8UAAAAADQ0QI_iZKmZidrPL7UYc085xXDf"
+                  sitekey={process.env.GATSBY_SITE_RECAPTCHA_KEY}
                   render="explicit"
                   verifyCallback={verifyCallback}
                   onloadCallback={recaptchaLoaded}
                 />
-                <p>
-                  <button type="submit" disabled={!isValidForm}>
-                    Send
-                  </button>
-                </p>
-              </form>
+              )}
             </FormSection>
             <SvgSection>
               <img src={AnimatedEnvelop} alt="Envelope" />
