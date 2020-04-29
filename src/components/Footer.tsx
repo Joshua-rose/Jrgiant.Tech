df73c87212ec4f4b35f94f79924c0544b01879a1
Joshua-Rose <joshua.rose1@outlook.com>
2020-04-29 15:42:38 -0500

    Create Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,121 @@
+/* eslint-disable max-len */
+import React, { ReactElement } from 'react'
+import { Link } from 'gatsby'
+import styled from '@emotion/styled'
+import { darken } from 'polished'
+import Links from '../templates/navigation'
+import JoshuaAtDesk from '../assets/JoshuaAtDesk.svg'
+import { colors, heights, breakpoints } from '../styles/variables'
+
+const TriangleSection = styled.div`
+  clip-path: polygon(0 100%, 100% 0, 100% 100%, 0% 100%);
+  background-color: ${colors.brand};
+  width: 100%;
+  margin: 0;
+  height: 50vh;
+  // position: relative;
+  position: absolute;
+  bottom: calc(${heights.header}px - 10px);
+  left: 0;
+  right: 0;
+  z-index: -1;
+  @media (max-width: ${breakpoints.lg}px) {
+    height: 300px;
+  }
+  @media (max-width: ${breakpoints.md}px) {
+    height: 190px;
+    margin: 0;
+  }
+`
+const StyledFooterBottom = styled.div`
+  height: ${heights.header}px;
+  background-color: ${colors.brand};
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+`
+
+const StyledFooter = styled.div`
+  position: relative;
+  height: 50vh;
+  display: flex;
+  @media (max-width: ${breakpoints.lg}px) {
+    height: calc(300px + ${heights.header}px);
+  }
+  @media (max-width: ${breakpoints.md}px) {
+    height: calc(190px + ${heights.header}px);
+  }
+`
+const FooterNav = styled.div`
+  flex: 3;
+  min-width: 200px;
+  text-align: right;
+  padding-top: 5rem;
+  a {
+    color: white;
+    padding: 1rem;
+    display: block;
+  }
+`
+const StyledImg = styled.svg`
+  flex: 2;
+  height: 100%;
+`
+function Footer(): ReactElement {
+  return (
+    <StyledFooter>
+      <TriangleSection />
+      <StyledFooterBottom />
+      <StyledImg width="474" height="434" viewBox="0 0 474 434" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <ellipse cx="240.5" cy="74" rx="54.5" ry="74" fill="#FFEEB4" />
+        <rect x="158" y="179" width="162" height="242" rx="46" fill="black" />
+        <rect className="shirt" x="174" y="185" width="132" height="199" rx="38" fill={darken(0.4, colors.brand)} />
+        <rect x="227" y="138" width="27" height="57" fill="#FFEEB4" />
+        <path d="M240.501 225L227.077 194.25H253.924L240.501 225Z" fill="#FFEEB4" />
+        <path
+          className="shirt"
+          d="M176.996 212.51C177.412 211.82 177.787 211.597 178.044 211.484C178.39 211.332 178.905 211.236 179.513 211.328C180.121 211.42 180.584 211.665 180.869 211.913C181.081 212.097 181.373 212.421 181.565 213.203L206.756 315.682C207.184 317.426 205.689 319.04 203.917 318.745L124.911 305.577C123.17 305.287 122.269 303.333 123.18 301.821L176.996 212.51Z"
+          stroke={darken(0.4, colors.brand)}
+          strokeWidth="35"
+        />
+        <path
+          className="shirt"
+          d="M300.004 210.51C299.588 209.82 299.213 209.597 298.956 209.484C298.61 209.332 298.095 209.236 297.487 209.328C296.879 209.42 296.416 209.665 296.131 209.913C295.919 210.097 295.627 210.421 295.435 211.203L270.244 313.682C269.816 315.426 271.311 317.04 273.083 316.745L352.089 303.577C353.83 303.287 354.731 301.333 353.82 299.821L300.004 210.51Z"
+          stroke={darken(0.4, colors.brand)}
+          strokeWidth="35"
+        />
+        <rect x="165" y="230" width="150" height="96" rx="10" fill="#BDBDBD" />
+        <rect x="18" y="338" width="434" height="96" fill="#7B770A" />
+        <g filter="url(#filter0_d)">
+          <line x1="10" y1="333" x2="464" y2="333" stroke="#302E05" strokeWidth="12" strokeLinecap="square" />
+        </g>
+        <path
+          d="M214 124C214 120.686 216.686 118 220 118H262C265.314 118 268 120.686 268 124V126C268 134.837 260.837 142 252 142H230C221.163 142 214 134.837 214 126V124Z"
+          stroke="#8F7C5E"
+          strokeWidth="8"
+        />
+        <ellipse cx="186.5" cy="75" rx="5.5" ry="14" fill="#FFEEB4" />
+        <ellipse cx="294.5" cy="74" rx="5.5" ry="14" fill="#FFEEB4" />
+        <rect x="192" y="62" width="40" height="16" rx="4" stroke="black" strokeWidth="4" />
+        <rect x="249" y="62" width="40" height="16" rx="4" stroke="black" strokeWidth="4" />
+        <line x1="232.982" y1="65.5" x2="247.018" y2="65.5" stroke="black" strokeWidth="4" />
+        <defs>
+          <filter id="filter0_d" x="0" y="327" width="474" height="20" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+          </filter>
+        </defs>
+      </StyledImg>
+      <FooterNav>{Links && Object.keys(Links).map(l => <Link to={Links[l]}>{l}</Link>)}</FooterNav>
+    </StyledFooter>
+  )
+}
+
+export default Footer
