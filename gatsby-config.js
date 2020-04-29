96dde88d36f280953418fe8d5ce4ce3e507369ac
Joshua-Rose <joshua.rose1@outlook.com>
2020-04-29 15:36:38 -0500

    Add Gatsby Recaptcha

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,9 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-recaptcha`
+    },
+    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
